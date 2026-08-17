923f2d7a39995cc49467ed9eb432aeca02acbe1e
sania359 <saniadhamniya@gmail.com>
2026-08-17 17:58:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var mentBtn = document.getElementById("menuBtn");
+var navbar = document.getElementById("navbar");
+
+menuBtn.addEventListener("click", function(){
+    navbar.classList.toggle("active");
+});
