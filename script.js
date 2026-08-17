45a75f488241f769640ab80dd2c2c7fdfbd207f2
sania359 <saniadhamniya@gmail.com>
2026-08-17 18:02:47 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var mentBtn = document.getElementById("menuBtn");
-var navbar = document.getElementById("navbar");
-
-menuBtn.addEventListener("click", function(){
-    navbar.classList.toggle("active");
-});
